3130a67b37179e1c45847ab3ba56f7d146c51d1e
Isroil Berdiyorov <isroilberdiyorov3@gmail.com>
2026-02-24 17:10:34 +0500

    fix logging for render

--- backend/Public/config/settings.py
+++ backend/Public/config/settings.py
@@ -174,10 +174,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = 'isroilberdiyorov3@gmail.com'
 EMAIL_HOST_PASSWORD = 'bjvw yyda ubfx urye'
 
